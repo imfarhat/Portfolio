9ce16d4fd2d8b9cc09db9920d7d264f755a3d177
imfarhat <ifarhat.dev@gmail.com>
2024-03-05 18:13:38 +0530

    Transition duration + drop shadow adjust

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,14 +25,14 @@ function Navbar() {
   }, []);
 
   const navLinkClassNormal =
-    "px-3 pt-1.5 pb-0.5 md:px-3 md:pb-1 flex flex-col md:flex-row md:gap-1.5 items-center justify-center rounded-t-xl font-semibold bg-white/10 text-white backdrop-blur-sm transition duration-200 ease-in active:bg-white/75 active:text-[#323262] hover:bg-white hover:text-[#323262]";
+    "px-3 pt-1.5 pb-0.5 md:px-3 md:pb-1 flex flex-col md:flex-row md:gap-1.5 items-center justify-center rounded-t-xl font-semibold bg-white/10 text-white backdrop-blur-sm transition duration-[250ms] ease-in active:bg-white/75 active:text-[#323262] hover:bg-white hover:text-[#323262]";
 
   const navLinkClassWide =
-    "px-1.5 pt-1.5 pb-0.5 md:px-2 md:pb-1 flex flex-col md:flex-row md:gap-1.5 items-center justify-center rounded-t-xl font-semibold bg-white/10 text-white backdrop-blur-sm transition duration-200 ease-in active:bg-white/75 active:text-[#323262] hover:bg-white hover:text-[#323262]";
+    "px-1.5 pt-1.5 pb-0.5 md:px-2 md:pb-1 flex flex-col md:flex-row md:gap-1.5 items-center justify-center rounded-t-xl font-semibold bg-white/10 text-white backdrop-blur-sm transition duration-[250ms] ease-in active:bg-white/75 active:text-[#323262] hover:bg-white hover:text-[#323262]";
 
   return (
-    <nav className="flex items-center justify-center sticky top-0 z-20 shadow-md">
-      <div className="flex gap-2 md:gap-4 items-center justify-between max-w-7xl mx-1 md:mx-2 w-full px-2 pt-2 pb-1 md:px-3 md:pt-3 md:pb-2 rounded-md rounded-t-none bg-gradient-to-tr from-indigo-500 via-purple-500 to-pink-500">
+    <nav className="flex items-center justify-center sticky top-0 z-20">
+      <div className="flex gap-2 md:gap-4 items-center justify-between max-w-7xl mx-1 md:mx-2 w-full px-2 pt-2 pb-1 md:px-3 md:pt-3 md:pb-2 rounded-md rounded-t-none bg-gradient-to-tr from-indigo-500 via-purple-500 to-pink-500 shadow-md">
         <Link
           to="/"
           className="font-semibold text-xl md:text-2xl text-white drop-shadow-md"
