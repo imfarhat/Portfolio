0a76dd46ce38b8ce31b6c0ea375b99d2d4ee4c62
imfarhat <ifarhat.dev@gmail.com>
2024-03-09 22:33:25 +0530

    tryiing to fix tailwind warning (4)

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
     "postcss-import": {},
-    "tailwindcss/nesting": {},
+    "tailwindcss/nesting": {}, // Enable nesting plugin before Tailwind
     tailwindcss: {},
     autoprefixer: {},
   },
