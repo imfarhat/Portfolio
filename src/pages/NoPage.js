d571be34fb5622e2dc5422fa884f7e4ba47f9003
imfarhat <ifarhat.dev@gmail.com>
2024-03-23 23:23:08 +0530

    chore: update 404 no page img

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header.js";
 import Footer from "../components/Footer.js";
-import { TbError404 } from "react-icons/tb";
+// import { TbError404 } from "react-icons/tb";
+import error404Img from "../assets/images/backgrounds/error404Img.png";
 
 function NoPage() {
   const [countdown, setCountdown] = useState(10);
@@ -29,8 +30,9 @@ function NoPage() {
     <>
       <Header />
       <main className="flex flex-col items-center justify-between gap-24 py-24 px-4 text-[#f5f5f5]/90 bg-[#16181d]">
-        <h2 className="text-3xl font-semibold">Sorry, page not found!</h2>
-        <TbError404 className="text-9xl scale-[3]" />
+        <img src={error404Img} alt="404" className="rounded" />
+        {/* <h2 className="text-3xl font-semibold">Sorry, page not found!</h2>
+        <TbError404 className="text-9xl scale-[3]" /> */}
         <div className="ficjc flex-col gap-2">
           <h3 className="text-lg">
             Redirecting to {previousPage ? "Previous" : "Home"} page in{" "}
@@ -38,7 +40,7 @@ function NoPage() {
           </h3>
           <button
             onClick={goBackOrHome}
-            className="text-base px-4 py-1 bg-[#f5f5f5] text-black font-semibold rounded active:bg-[#f5f5f5]/75 transition active:scale-95 md:duration-200 ease-in"
+            className="text-base px-5 py-1 border-2 text-[#f5f5f5] hover:bg-[#f5f5f5] hover:text-[#16181d] font-semibold rounded active:bg-[#f5f5f5]/75 transition active:scale-95 md:duration-200 ease-in"
           >
             {previousPage ? "Go Back" : "Go Home"}
           </button>
