bcc866e56d2fa42c9a3b2aff4d57a3cc4b6625cc
imfarhat <ifarhat.dev@gmail.com>
2024-03-24 06:30:26 +0530

    chore: remove unnecessary asset and code

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -2,8 +2,7 @@ import React, { useState, useEffect, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header.js";
 import Footer from "../components/Footer.js";
-// import { TbError404 } from "react-icons/tb";
-import error404Img from "../assets/images/backgrounds/error404Img.png";
+import { TbFaceIdError } from "react-icons/tb";
 
 function NoPage() {
   const [countdown, setCountdown] = useState(10);
@@ -30,17 +29,16 @@ function NoPage() {
     <>
       <Header />
       <main className="flex flex-col items-center justify-between gap-8 py-4 md:pt-8 pb-16 text-[#f5f5f5]/90 bg-[#16181d]">
-        <img src={error404Img} alt="404" className="rounded" />
-        {/* <h2 className="text-3xl font-semibold">Sorry, page not found!</h2>
-        <TbError404 className="text-9xl scale-[3]" /> */}
+        <h2 className="text-lg font-semibold">Sorry, page not found!</h2>
+        <TbFaceIdError className="text-9xl" />
         <div className="ficjc flex-col gap-2">
-          <h3 className="text-lg">
+          <h3>
             Redirecting to {previousPage ? "Previous" : "Home"} page in{" "}
             {countdown}s...
           </h3>
           <button
             onClick={goBackOrHome}
-            className="text-base px-5 py-1 border-2 text-[#f5f5f5] hover:bg-[#f5f5f5] hover:text-[#16181d] active:text-[#16181d] font-semibold rounded active:bg-[#f5f5f5]/75 transition active:scale-95 md:duration-200 ease-in"
+            className="text-base px-4 py-1 border-2 text-[#f5f5f5] hover:bg-[#f5f5f5] hover:text-[#16181d] active:text-[#16181d] font-semibold rounded active:bg-[#f5f5f5]/75 transition active:scale-95 md:duration-200 ease-in"
           >
             {previousPage ? "Go Back" : "Go Home"}
           </button>
