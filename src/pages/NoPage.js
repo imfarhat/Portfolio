38a585ced59a43a56d47f5592dfdbe2eeee96dff
imfarhat <ifarhat.dev@gmail.com>
2024-03-26 00:15:11 +0530

    stye: 404 error page

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -5,7 +5,7 @@ import Footer from "../components/Footer.js";
 import { TbFaceIdError } from "react-icons/tb";
 
 function NoPage() {
-  const [countdown, setCountdown] = useState(10);
+  const [countdown, setCountdown] = useState(10000000);
   const navigate = useNavigate();
   const previousPage = sessionStorage.getItem("previousPage");
 
@@ -28,18 +28,15 @@ function NoPage() {
   return (
     <>
       <Header />
-      <main className="flex flex-col items-center justify-between gap-8 py-4 md:pt-8 pb-16 text-[#f5f5f5]/90 bg-[#16181d]">
+      <main className="flex flex-col items-center justify-between gap-16 py-32 text-[#f5f5f5]/90 bg-[#16181d]">
         <h2 className="text-lg font-semibold">Sorry, page not found!</h2>
-        <TbFaceIdError className="text-9xl" />
-        <div className="ficjc flex-col gap-4">
+        <TbFaceIdError className="text-9xl text-[#f5f5f5]/75 scale-125" />
+        <div className="ficjc flex-col gap-6">
           <span className="text-sm">
             Redirecting to {previousPage ? "Previous" : "Home"} page in{" "}
             {countdown}s...
           </span>
-          <button
-            onClick={goBackOrHome}
-            className="text-base px-4 py-1 border text-[#f5f5f5] hover:bg-[#f5f5f5] hover:text-[#16181d] active:text-[#16181d] font-semibold rounded active:bg-[#f5f5f5]/75 transition active:scale-95 md:duration-200 ease-in"
-          >
+          <button onClick={goBackOrHome} className="back-or-home-btn">
             {previousPage ? "Go Back" : "Go Home"}
           </button>
         </div>
