ddbd5e603b9b9115a454b7207a06c3a74a34aeab
imfarhat <ifarhat.dev@gmail.com>
2024-03-26 05:34:49 +0530

    feat: share website with logo desription and title

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -4,20 +4,43 @@ import { GiShare } from "react-icons/gi";
 const ShareButton = () => {
   const [shareUrl, setShareUrl] = useState("");
   const [shareTitle, setShareTitle] = useState("");
+  const [shareDescription, setShareDescription] = useState("");
+  const shareImage = "/android-chrome-512x512.png"; // Path to the image in the public folder
 
   useEffect(() => {
-    setShareUrl(window.location.href);
-    setShareTitle(document.title);
+    setShareUrl(window.location.origin);
+    setShareTitle(document.title + " Web App");
+
+    // Get the description from the meta tag
+    const descriptionMetaTag = document.querySelector(
+      'meta[name="description"]'
+    );
+    if (descriptionMetaTag) {
+      setShareDescription(descriptionMetaTag.content);
+    }
   }, []);
 
   const handleShare = async () => {
     try {
       if (navigator.share) {
-        await navigator.share({
+        const data = {
           title: shareTitle,
+          text: `${shareDescription}\n${shareUrl}`, // Include description and URL
           url: shareUrl,
+          files: [], // Initialize files array
+        };
+
+        // Fetch the image file
+        const response = await fetch(shareImage);
+        const blob = await response.blob();
+        const file = new File([blob], "android-chrome-512x512.png", {
+          type: "image/png",
         });
-        // console.log("Shared successfully");
+
+        // Add the file to the files array
+        data.files.push(file);
+
+        await navigator.share(data);
       } else {
         throw new Error("Web Share API not supported");
       }
@@ -27,7 +50,7 @@ const ShareButton = () => {
   };
 
   return (
-    <button onClick={handleShare} title="Copy URL" className="header-promo-btn">
+    <button onClick={handleShare} title="Share" className="header-promo-btn">
       <GiShare />
     </button>
   );
