a4a221728d0c131dff021da2aeb372c480bc3ee8
imfarhat <ifarhat.dev@gmail.com>
2024-03-26 18:22:11 +0530

    style: home page paddings

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,8 +17,8 @@ function Home() {
   return (
     <>
       <Header />
-      <main className="py-4 md:pt-8 pb-16 ficjc flex-col gap-8 bg-[#16181d]">
-        <section className="pt-8 pb-16 md:pb-24 max-w-7xl w-full h-full flex flex-col gap-8">
+      <main className="pt-4 pb-8 md:pt-8 md:pb-16 ficjc flex-col gap-8 bg-[#16181d]">
+        <section className="max-w-7xl w-full h-full flex flex-col gap-8">
           <article className="flex flex-col-reverse md:flex-row items-center justify-between gap-4 rounded-md overflow-hidden">
             <aside className="flex flex-col items-start justify-center gap-2 w-full md:w-[50%] p-2 md:p-4 h-full text-[#f5f5f5]">
               <h1 className="text-3xl font-bold">
