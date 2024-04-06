c41a6ef4a9dba6b5f9310435ca3f6d2b1b30376f
imfarhat <ifarhat.dev@gmail.com>
2024-04-06 16:37:13 +0530

    chore: 404 error page update

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -29,8 +29,11 @@ function NoPage() {
     <>
       <Header />
       <main className="flex flex-col items-center justify-between gap-16 py-32 text-[#f5f5f5]/90 bg-[#16181d]">
-        <h2 className="text-2xl font-semibold">Sorry, page not found!</h2>
-        <TbFaceIdError className="text-9xl text-[#f5f5f5]/75 scale-125" />
+        <h2 className="text-xl font-semibold">Sorry, page not found!</h2>
+        <img
+          src="https://raw.githubusercontent.com/imfarhat/Portfolio/51f54ae396db7f6aa2039ff95a15569483d382d6/src/assets/images/backgrounds/404-spacescraft.svg"
+          alt="404 "
+        />
         <div className="ficjc flex-col gap-6">
           <span>
             Redirecting to {previousPage ? "Previous" : "Home"} page in{" "}
