ad9f515a38b05dc6e2a212ac8a9eb31461245f2e
I Farhat <ifarhat.dev@gmail.com>
2024-10-16 10:21:19 +0530

    adjust height and font

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -59,43 +59,43 @@ function Home() {
             </aside>
           </article>
 
-          <article className="flex flex-col lg:flex-row-reverse items-center justify-center lg:justify-between gap-4 bg-gradient-to-r from-[#16181d] to-[#646674]/25 md:rounded-md overflow-hidden p-2 md:p-4">
+          <article className="flex flex-col lg:flex-row-reverse overflow-x-auto items-center justify-center lg:justify-between gap-4 bg-gradient-to-r from-[#16181d] to-[#646674]/25 md:rounded-md overflow-hidden p-2 md:p-4">
             <h1 className="rotate-0 lg:rotate-90 text-[#f5f5f5] text-2xl md:text-3xl font-bold">
               Skills
             </h1>
             <div className="flex flex-wrap items-center justify-center gap-2 md:gap-4">
               <h3 className="programming-lang-card">
-                <FaHtml5 className="text-5xl md:text-9xl text-[#e34c26]/95 bg-[#f5f5f5] rounded-[0.175rem] md:rounded-md aspect-square" />
+                <FaHtml5 className="text-7xl md:text-9xl text-[#e34c26]/95 bg-[#f5f5f5] rounded-[0.175rem] md:rounded-md aspect-square" />
                 <span className="text-[0.45rem] md:font-semibold md:text-base pt-0.5">
                   HTML5
                 </span>
               </h3>
               <h3 className="programming-lang-card">
-                <FaCss3Alt className="text-5xl md:text-9xl text-[#264de4]/95 bg-[#f5f5f5] rounded-[0.175rem] md:rounded-md aspect-square" />
+                <FaCss3Alt className="text-7xl md:text-9xl text-[#264de4]/95 bg-[#f5f5f5] rounded-[0.175rem] md:rounded-md aspect-square" />
                 <span className="text-[0.45rem] md:font-semibold md:text-base pt-0.5">
                   CSS3
                 </span>
               </h3>
               <h3 className="programming-lang-card">
-                <SiTailwindcss className="text-5xl md:text-9xl text-sky-500 bg-[#f5f5f5] rounded-[0.175rem] md:rounded-md aspect-square p-1" />
+                <SiTailwindcss className="text-7xl md:text-9xl text-sky-500 bg-[#f5f5f5] rounded-[0.175rem] md:rounded-md aspect-square p-1" />
                 <span className="text-[0.45rem] md:font-semibold md:text-base pt-0.5">
                   Tailwind
                 </span>
               </h3>
               <h3 className="programming-lang-card">
-                <SiJavascript className="text-5xl md:text-9xl text-[#f0db4f] rounded-[0.175rem] md:rounded-md aspect-square" />
+                <SiJavascript className="text-7xl md:text-9xl text-[#f0db4f] rounded-[0.175rem] md:rounded-md aspect-square" />
                 <span className="text-[0.45rem] md:font-semibold md:text-base pt-0.5">
                   JavaScript
                 </span>
               </h3>
               <h3 className="programming-lang-card">
-                <FaReact className="text-5xl md:text-9xl text-sky-400 bg-[#f5f5f5] rounded-[0.175rem] md:rounded-md aspect-square p-1" />
+                <FaReact className="text-7xl md:text-9xl text-sky-400 bg-[#f5f5f5] rounded-[0.175rem] md:rounded-md aspect-square p-1" />
                 <span className="text-[0.45rem] md:font-semibold md:text-base pt-0.5">
                   React JS
                 </span>
               </h3>
               <h3 className="programming-lang-card">
-                <SiNextdotjs className="text-5xl md:text-9xl text-black bg-[#f5f5f5] rounded-[0.175rem] md:rounded-md aspect-square p-1" />
+                <SiNextdotjs className="text-7xl md:text-9xl text-black bg-[#f5f5f5] rounded-[0.175rem] md:rounded-md aspect-square p-1" />
                 <span className="text-[0.45rem] md:font-semibold md:text-base pt-0.5">
                   Next JS
                 </span>
